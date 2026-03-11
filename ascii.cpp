7c3801dbd6e81b38d76bbf06383a6eebac8ba233
kimminji2302 <kimminji2302@gmail.com>
2026-03-11 22:13:07 +0900

    assignment

---
+++ ascii.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "===== MY INITIAL (MJ) =====" << endl;
+    cout << endl;
+
+    cout << "M   M     JJJJJ" << endl;
+    cout << "MM MM       J" << endl;
+    cout << "M M M       J" << endl;
+    cout << "M   M    J  J" << endl;
+    cout << "M   M     JJ" << endl;
+
+    cout << endl;
+    cout << "===== DOG ASCII IMAGE =====" << endl;
+    cout << endl;
+
+    cout << "        @@@@@@@@@@@@@        " << endl;
+    cout << "     @@@@@@@@@@@@@@@@@@@     " << endl;
+    cout << "   @@@@@@@@@@@@@@@@@@@@@@@   " << endl;
+    cout << "  @@@@@@@@  @@@@@  @@@@@@@@  " << endl;
+    cout << " @@@@@@@      @@      @@@@@@ " << endl;
+    cout << "@@@@@@   @@        @@   @@@@@" << endl;
+    cout << "@@@@@      @@@@@@@@      @@@@" << endl;
+    cout << "@@@@@    @@@@@@@@@@@@    @@@@" << endl;
+    cout << " @@@@@      @@@@@@      @@@@ " << endl;
+    cout << "  @@@@@@            @@@@@@   " << endl;
+    cout << "    @@@@@@@@@@@@@@@@@@@      " << endl;
+    cout << "       @@@@@@@@@@@@@         " << endl;
+
+    cout << endl;
+    cout << "ASCII Dog Complete!" << endl;
+
+    return 0;
+}
+
